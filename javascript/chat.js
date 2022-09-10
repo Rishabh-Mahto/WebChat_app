402e723cbf78590d3c7e7df30d78bc15f93b7bb2
Rishabh Mahto <itsrishabh13@gmail.com>
2022-09-11 01:05:38 +0530

    WebChat_app

---
+++ javascript/chat.js
@@ -0,0 +1,58 @@
+const form = document.querySelector(".typing-area"),
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit =(e) =>{
+    e.preventDefault(); //preventing form from submitting
+    }
+
+sendBtn.onclick = ()=>{
+    //starting Ajax
+let xhr = new XMLHttpRequest();  //CREATING XML object
+xhr.open("POST", "php/insert-chat.php", true);
+xhr.onload =() =>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+            inputField.value = ""; //once message is inserted into the database then leave blank the input field
+            scrollToBottom();
+        }
+    }
+}
+    //we have to send the form data through ajax to php
+    let formData = new FormData(form); //creating new formData Object
+    xhr.send(formData); //sending the form data to php
+}
+
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+
+setInterval(() =>{
+    //starting Ajax
+    let xhr = new XMLHttpRequest();  //CREATING XML object
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload =() =>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){  //if active class not contains in chat box to scroll to bottom
+                    scrollToBottom();
+                }
+            }
+        }
+    }
+    //we have to send the form data through ajax to php
+    let formData = new FormData(form); //creating new formData Object
+    xhr.send(formData); //sending the form data to php
+}, 500); //this function will run frequently after 500ms
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
